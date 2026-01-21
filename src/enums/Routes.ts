51c54520fec5f3c346eae91b3e1b3d53dca8f25e
Daniel <palfidani4@gmail.com>
2026-01-21 17:10:51 +0200

    Remove old website

--- src/enums/Routes.ts
+++
@@ -1,9 +0,0 @@
-export const Routes = {
-  Home: "/",
-  Contact: "/contact",
-  Skills: "/skills",
-  PersonalProjects: "/personal-projects",
-  ProfessionalExperience: "/professional-experience",
-} as const;
-
-export type Routes = (typeof Routes)[keyof typeof Routes];
